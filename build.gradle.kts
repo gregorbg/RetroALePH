5aacab102184f607f5ebd675bbf93a37645cc5cf
Gregor Billing <gregor.billing.dev@gmail.com>
2023-04-04 14:55:26 +0200

    Update Kotlin to 1.8.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
 }
 
 group = "de.uzk.oas.japan"
